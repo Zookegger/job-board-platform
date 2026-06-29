19cb66b0ecfc942173f4826b81579b658985f5d6
Chun <phamquanghieu100804@gmail.com>
2026-06-29 16:36:37 +0700

    test: update NotificationRepositoryTest to use SpringBootTest with Testcontainers

--- server/src/test/java/com/yoedu/job_board_platform/repositories/NotificationRepositoryTest.java
+++ server/src/test/java/com/yoedu/job_board_platform/repositories/NotificationRepositoryTest.java
@@ -1,11 +1,10 @@
 package com.yoedu.job_board_platform.repositories;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
